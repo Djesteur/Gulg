a69a1a08ebcdf9bb191fd50f86d9b520e0af9826
Fabre Guillaume <guillaume.fabre03@etu.umontpellier.fr>
2019-08-13 18:13:05 +0200

    Begin of collision system. Only circle for th moment, with a nice exemple

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -6,13 +6,19 @@
 #include "GulgEngine/GulgEngine.hpp"
 
 #include "Systems/Graphics/CharacterDraw.hpp"
+#include "Systems/Graphics/PhysicTestDrawCircle.hpp"
 #include "Systems/CharacterUpdate.hpp"
+#include "Systems/Physics/Physic.hpp"
+#include "Systems/Physics/PhysicTest.hpp"
 
 #include "Game/Camera.hpp"
 
 #include "Components/Graphics/AnimationSet.hpp"
+#include "Components/Graphics/CircleShape.hpp"
+#include "Components/Physics/Hitbox.hpp"
 
-#include "Systems/Physics/Physic.hpp"
+#include "PhysicTest.hpp"
+
 
 int main() {
 
@@ -29,9 +35,15 @@ int main() {
 
 		CharacterUpdate updateChar{engine};
 		CharacterDraw drawChar{window, engine};
-		Physic physicSystem{engine};
+		Gg::Systems::Physic physicSystem{engine};
+		PhysicTestDrawCircle drawCircleTest{window, engine};
+		Gg::Systems::PhysicTest physicTestUpdates{engine};
 
-		Gg::Entity character{engine.getNewEntity()};
+		std::vector<Gg::Entity> circles = randomCircles(engine, physicSystem, drawCircleTest, physicTestUpdates, 50);
+
+		//Character
+
+		/*Gg::Entity character{engine.getNewEntity()};
 
 		std::shared_ptr<Gg::Component::AnimationSet> anim{std::make_shared<Gg::Component::AnimationSet>()};
 		if(!anim->loadFromFile(engine, "Datas/CharAnimation.xml")) { return 0; }
@@ -42,16 +54,26 @@ int main() {
 		std::shared_ptr<Gg::Component::Vector2D> graphicPosition{std::make_shared<Gg::Component::Vector2D>()};
 		std::shared_ptr<Gg::Component::Vector2D> physicPosition{std::make_shared<Gg::Component::Vector2D>()};
 		std::shared_ptr<Gg::Component::Vector2D> speed{std::make_shared<Gg::Component::Vector2D>()};
+		std::shared_ptr<Gg::Component::UnsignedInt> mass{std::make_shared<Gg::Component::UnsignedInt>(1)};
 		engine.addComponentToEntity(character, "GraphicPosition", std::static_pointer_cast<Gg::Component::AbstractComponent>(graphicPosition));
 		engine.addComponentToEntity(character, "PhysicPosition", std::static_pointer_cast<Gg::Component::AbstractComponent>(physicPosition));
 		engine.addComponentToEntity(character, "Speed", std::static_pointer_cast<Gg::Component::AbstractComponent>(speed));
+		engine.addComponentToEntity(character, "Mass", std::static_pointer_cast<Gg::Component::AbstractComponent>(mass));
 		
-
+		std::shared_ptr<Gg::Component::CircleHitbox> hitbox{std::make_shared<Gg::Component::CircleHitbox>()};
+		std::shared_ptr<Gg::Component::CircleShape> graphicHitbox{std::make_shared<Gg::Component::CircleShape>()};
+		engine.addComponentToEntity(character, "Hitbox", std::static_pointer_cast<Gg::Component::AbstractComponent>(hitbox));
+		engine.addComponentToEntity(character, "GraphicHitbox", std::static_pointer_cast<Gg::Component::AbstractComponent>(graphicHitbox));
 		
+		hitbox->radius = 3.f;
+		graphicHitbox->circle.setRadius(hitbox->radius*8.f);
+		graphicHitbox->circle.setOrigin(hitbox->radius*8.f, hitbox->radius*8.f);
+		graphicHitbox->circle.setFillColor(sf::Color{255, 0, 255});
 
-		physicSystem.addEntity(character);
 		updateChar.addEntity(character);
 		drawChar.addEntity(character);
+		physicSystem.addEntity(character);*/
+
 
 		bool pressedUp{false}, pressedDown{false}, pressedLeft{false}, pressedRight{false};
 
@@ -63,8 +85,25 @@ int main() {
 	        while(window.pollEvent(event)) {
 
 	            if(event.type == sf::Event::Closed) { window.close(); }
-
 	            if(event.type == sf::Event::KeyPressed) {
+
+	            	 if(event.key.code == sf::Keyboard::Space) { 
+ 
+				    	std::default_random_engine randomEngine { std::time(nullptr) };
+    					std::uniform_real_distribution<float> speedRandom(-30, 30);
+
+		            	for(Gg::Entity currentCicle: circles) {
+
+		            		std::shared_ptr<Gg::Component::Vector2D> circleSpeed{ 
+								std::static_pointer_cast<Gg::Component::Vector2D>(engine.getComponent(currentCicle, "Speed"))
+							};
+
+							circleSpeed->value = sf::Vector2f{speedRandom(randomEngine), speedRandom(randomEngine)};
+		            	}
+				    }
+				}
+
+	            /*if(event.type == sf::Event::KeyPressed) {
 
 	            	std::shared_ptr<Gg::Component::Vector2D> charSpeed{ 
 						std::static_pointer_cast<Gg::Component::Vector2D>(engine.getComponent(character, "Speed"))
@@ -145,13 +184,14 @@ int main() {
 				    	charSpeed->value -= sf::Vector2f{16.f, 0.f};
 				    	pressedRight = false;
 				    }
-				}
+				}*/
 			}
 
 			//Update
 
 			physicSystem.applyAlgorithms();
 			updateChar.applyAlgorithms();
+			physicTestUpdates.applyAlgorithms();
 
 			//Draw
 
@@ -159,18 +199,10 @@ int main() {
 	        window.setView(currentView);
 	        window.setView(window.getDefaultView());
 	        drawChar.applyAlgorithms();
+	        drawCircleTest.applyAlgorithms();
 	        window.display();
 	    }
 	}
 
 	return 0;
 }
-
-/*
-
-	- Position
-	- Vitesse
-	- Masse
-	- Hitbox
-	
-*/
